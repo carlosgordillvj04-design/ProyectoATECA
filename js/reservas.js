efee730be692d00c55c6c7862b5fc1a36208ec18
carlosgordillvj04-design <carlosgordillvj04@gmail.com>
2026-05-25 12:13:24 +0200

    Terminando la parte de reservas html

--- js/reservas.js
+++ js/reservas.js
@@ -3,15 +3,23 @@ $(function () {
 
     const hoy = new Date().toISOString().split("T")[0]
 
-    inputDate.attr("min",hoy)
+    inputDate.attr("min", hoy)
 
     $(document).on('click', '.btn-reservar', function () {
         const fecha = $("#fecha").val()
-        if ( fecha != "" ) {
+        if (fecha != "") {
             alert($(this).val() + " <-> " + fecha)
-        }else {
+            const elemento = $(`table tr:nth-child(${$(this).val()})`)
+            console.log(elemento);
+            elemento.find("span").addClass("reservado").html("reservado")
+            $(this).html("cancelar")
+        } else {
             alert("Debes elegir una fecha")
         }
+    })
+
+    $(".btn-verReservas").on('click', function () {
+        alert("Click this!")
     })
 
 })
